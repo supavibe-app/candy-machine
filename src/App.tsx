9534c695e9ce3b2b4dffc6196c79a304bdcdc4ed
Ahmad Taufiq <ataufiq665@gmail.com>
2022-01-20 10:17:13 +0700

    three button mint

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,59 @@
-import './App.css';
-import { useMemo } from 'react';
-import * as anchor from '@project-serum/anchor';
-import Home from './Home';
+import "./App.css";
+import { useMemo } from "react";
+import * as anchor from "@project-serum/anchor";
+import Home from "./Home";
 
-import { clusterApiUrl } from '@solana/web3.js';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { clusterApiUrl } from "@solana/web3.js";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import {
   getPhantomWallet,
   getSlopeWallet,
   getSolflareWallet,
   getSolletWallet,
   getSolletExtensionWallet,
-} from '@solana/wallet-adapter-wallets';
+} from "@solana/wallet-adapter-wallets";
 
 import {
   ConnectionProvider,
   WalletProvider,
-} from '@solana/wallet-adapter-react';
-import { WalletDialogProvider } from '@solana/wallet-adapter-material-ui';
+} from "@solana/wallet-adapter-react";
+import { WalletDialogProvider } from "@solana/wallet-adapter-material-ui";
 
-import { ThemeProvider, createTheme } from '@material-ui/core';
+import { ThemeProvider, createTheme } from "@material-ui/core";
 
 const theme = createTheme({
   palette: {
-    type: 'dark',
+    type: "dark",
   },
 });
 
-const getCandyMachineId = (): anchor.web3.PublicKey | undefined => {
+const getCandyMachineId = (
+  candyMachineID: string
+): anchor.web3.PublicKey | undefined => {
   try {
-    const candyMachineId = new anchor.web3.PublicKey(
-      process.env.REACT_APP_CANDY_MACHINE_ID!,
-    );
+    const candyMachineId = new anchor.web3.PublicKey(candyMachineID!);
 
     return candyMachineId;
   } catch (e) {
-    console.log('Failed to construct CandyMachineId', e);
+    console.log("Failed to construct CandyMachineId", e);
     return undefined;
   }
 };
 
-const candyMachineId = getCandyMachineId();
+const candyMachineIdSilver = getCandyMachineId(
+  process.env.REACT_APP_CANDY_MACHINE_ID_SILVER!
+);
+const candyMachineIdGold = getCandyMachineId(
+  process.env.REACT_APP_CANDY_MACHINE_ID_GOLD!
+);
+const candyMachineIdPlatinum = getCandyMachineId(
+  process.env.REACT_APP_CANDY_MACHINE_ID_PLATINUM!
+);
 const network = process.env.REACT_APP_SOLANA_NETWORK as WalletAdapterNetwork;
 const rpcHost = process.env.REACT_APP_SOLANA_RPC_HOST!;
-const connection = new anchor.web3.Connection(rpcHost
-  ? rpcHost
-  : anchor.web3.clusterApiUrl('devnet'));
+const connection = new anchor.web3.Connection(
+  rpcHost ? rpcHost : anchor.web3.clusterApiUrl("devnet")
+);
 
 const startDateSeed = parseInt(process.env.REACT_APP_CANDY_START_DATE!, 10);
 const txTimeoutInMilliseconds = 30000;
@@ -61,7 +69,7 @@ const App = () => {
       getSolletWallet({ network }),
       getSolletExtensionWallet({ network }),
     ],
-    [],
+    []
   );
 
   return (
@@ -70,7 +78,9 @@ const App = () => {
         <WalletProvider wallets={wallets} autoConnect>
           <WalletDialogProvider>
             <Home
-              candyMachineId={candyMachineId}
+              candyMachineIdSilver={candyMachineIdSilver}
+              candyMachineIdGold={candyMachineIdGold}
+              candyMachineIdPlatinum={candyMachineIdPlatinum}
               connection={connection}
               startDate={startDateSeed}
               txTimeout={txTimeoutInMilliseconds}
